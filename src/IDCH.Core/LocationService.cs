d3fc15f74a08fffa537fbc997555026ecc90f5fc
muhammad ibnu fadhil <mifmasterz@gmail.com>
2022-09-20 16:02:07 +0700

    add others

---
+++ src/IDCH.Core/LocationService.cs
@@ -0,0 +1,34 @@
+﻿using IDCH.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace IDCH.Core
+{
+    public class LocationService
+    {
+        public static string LIST_LOCATIONS = "config/locations";
+
+        CoreService core;
+        public LocationService(CoreService core)
+        {
+            this.core = core;
+        }
+        public async Task<LocationObj[]> GetLocations()
+        {
+            return await core.CallServiceAndSerialize<LocationObj[]>(LIST_LOCATIONS, null);
+        }
+
+
+
+
+    }
+
+
+
+}
---
+++ src/IDCH.Core/TokenService.cs
@@ -0,0 +1,34 @@
+﻿using IDCH.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace IDCH.Core
+{
+    public class TokenService
+    {
+        public static string LIST_TOKEN = "user-resource/token/list";
+
+        CoreService core;
+        public TokenService(CoreService core)
+        {
+            this.core = core;
+        }
+        public async Task<ListTokenObj[]> GetListToken()
+        {
+            return await core.CallServiceAndSerialize<ListTokenObj[]>(LIST_TOKEN, null);
+        }
+
+      
+
+
+    }
+
+
+
+}
